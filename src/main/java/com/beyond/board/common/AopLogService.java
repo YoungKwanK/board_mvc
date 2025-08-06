b37fd5adf1bbdede510185490b0159008e537326
YoungKwanK <young.kwin@gmail.com>
2025-08-06 10:04:30 +0900

    2025-08-06_1

---
+++ src/main/java/com/beyond/board/common/AopLogService.java
@@ -0,0 +1,54 @@
+package com.beyond.board.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Aspect // aop관련 코드임을 명시
+@Component
+@Slf4j
+public class AopLogService {
+    
+//    aop의 대상(공통 작업)이 되는 controller, service 등의 위치를 명시
+    @Pointcut("within(@org.springframework.stereotype.Controller *)")
+    public void controllerPointcut() {
+        
+    }
+    
+//    controller에 진입하기 전에 사용자 요청을 log출력. 핵심 로직 수행 후 log 출력.
+//    방법1. around 어노테이션을 통해 controller에 걸쳐져 있는 코드 패턴 사용
+    @Around("controllerPointcut()")
+//    joinPoint는 사용자가 실행하려고 하는 코드를 의미하고, 위에서 정의한 pointcut을 의미
+    public Object controllerLogger(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("aop start");
+
+//        대상이 되는 controller 로직 수행
+        Object object = joinPoint.proceed();
+
+        log.info("aop end");
+        log.info("method 명 : " + joinPoint.getSignature().getName());
+
+//        직접 HttpServletRequest객체에서 사용자 요청정보
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        log.info("HTTP 메서드 : " + request.getMethod());
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.valueToTree(parameterMap);
+        log.info("사용자 input값 : " + objectNode);
+        return object;
+    }
+
+//    방법2. Before, After어노테이션 사용
+    
+}
